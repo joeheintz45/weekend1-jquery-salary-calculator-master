b102c75ce55342810f4b273e3a08f849a67cb184
Joey Heintz <joeheintz45@gmail.com>
2020-10-09 16:22:07 -0500

    made button store input vals into variables

--- scripts/client.js
+++ scripts/client.js
@@ -2,4 +2,18 @@ console.log('hey');
 
 $(document).ready(onReady);
 
-function onReady() {}
+function onReady() {
+  console.log('in onReady');
+
+  $('.js-submit-btn').on('click', grabForm);
+}
+
+function grabForm() {
+  const firstName = $('.js-first-name').val();
+  const lastName = $('.js-last-name').val();
+  const idNum = $('.js-id-number').val();
+  const title = $('.js-title').val();
+  const salary = $('.js-salary').val();
+
+  console.log(firstName, lastName, idNum, title, salary);
+}
